d3dc779d60b2fe9094d6baa1266db213636ea983
Bruce Bales <bruce.morgan.bales@gmail.com>
2019-09-23 11:25:03 -0700

    Create worker function for editing band entries

--- src/internal/access/band.go
+++ src/internal/access/band.go
@@ -8,7 +8,16 @@ import (
 	"github.com/brucebales/bandmanager-backend/src/internal/structs"
 )
 
+//CreateBandJob is the struct passed into the band creation channel
 type CreateBandJob struct {
+	Name        string
+	Description string
+	User        structs.User
+}
+
+//EditBandJob is the struct passed into the band edit channel
+type EditBandJob struct {
+	ID          int
 	Name        string
 	Description string
 	User        structs.User
@@ -17,7 +26,6 @@ type CreateBandJob struct {
 //CreateBandWorker creates new bands in the DB and adds the creator as the first member.
 //This function runs in async using the CreateBandJob struct.
 func CreateBandWorker(j <-chan CreateBandJob, wg *sync.WaitGroup, db *sql.DB) error {
-
 	for job := range j {
 		res, err := db.Exec("INSERT INTO prim.bands(name, description) VALUES(?, ?);", job.Name, job.Description)
 		if err != nil {
@@ -30,6 +38,40 @@ func CreateBandWorker(j <-chan CreateBandJob, wg *sync.WaitGroup, db *sql.DB) er
 		_, err = db.Exec("INSERT INTO prim.bands_members(user_id, band_id, name, role, acl) VALUES(?, ?, ?, ?, ?);", job.User.ID, bandid, job.User.Name, "Founder", 4)
 		if err != nil {
 			fmt.Println("Could not create band: ", err)
+		}
+	}
+	wg.Done()
+	return nil
+}
+
+//EditBandWorker updates band information using info received in the EditBandJob channel
+func EditBandWorker(j <-chan EditBandJob, wg *sync.WaitGroup, db *sql.DB) error {
+	for job := range j {
+		//Check that user requesting change is permitted to do so
+		//TODO: Split these ACL checks into their own function
+		members := make([]structs.Member, 0)
+		rows, err := db.Query("SELECT user_id, name, role, acl FROM prim.bands_members WHERE band_id = ?", job.ID)
+		for rows.Next() {
+			memb := structs.Member{}
+			err = rows.Scan(&memb.UserID, &memb.Name, &memb.Role, &memb.ACL)
+			if err != nil {
+				return err
+			}
+			members = append(members, memb)
+		}
+		var permitted = false
+		for _, m := range members {
+			if m.UserID == job.User.ID && m.ACL >= 3 {
+				permitted = true
+			}
+		}
+		if permitted {
+			_, err = db.Exec("UPDATE prim.bands(name, description) VALUES(?, ?) WHERE id = ?;", job.Name, job.Description, job.ID)
+			if err != nil {
+				fmt.Println("Could not edit band: ", err)
+			}
+		} else {
+			fmt.Println("User requested unauthorized update: ", job.User.ID)
 		}
 	}
 	wg.Done()
